069f6fb26ce48c8350a1f8100475de35418eb83f
Lawson Chibueze <speak2lawson@gmail.com>
2021-07-13 09:15:13 +0100

    first backend commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1db81bbbb5d0d3694434356971985535'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '55c7c3ae-7c18-45a5-a518-909b4bbcda3d'
+};
